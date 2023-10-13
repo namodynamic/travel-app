454626f82dc3b461e94265ea41b9903f310f2513
namodynamic <nnamdi4u09@gmail.com>
2023-10-14 00:23:34 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel UL/UX for camping",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <NavBar />
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
